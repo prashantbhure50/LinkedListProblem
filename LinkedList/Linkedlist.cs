4ce5707ea8cc90480e23e6ee2cc29efc70d36f29
Prashant Bhure <Prashantbhure50@gmail.com>
2021-05-05 20:13:13 +0530

    Add project files.

---
+++ LinkedList/Linkedlist.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Linkedlist
+    {
+        internal Node head;
+
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if(this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list",node.data);
+
+        }
+       
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Linkedlist list = new Linkedlist();
+            list.Add(50);
+            list.Add(36);
+            list.Add(70);
+
+            
+            Console.ReadKey();
+
+
+        }
+    }
+}
